a2c9481eb56c0c6e78b811b8750a53edd93af1cf
iman irandoost <imn.irdst@gmail.com>
2016-12-24 09:59:19 +0330

    Add display configuration scripts

---
+++ Applications/scripts-imn/display-mirror.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI1 --off --output VGA-1-2 --off --output LVDS1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output VGA1 --mode 1360x768 --pos 0x0 --rotate normal
